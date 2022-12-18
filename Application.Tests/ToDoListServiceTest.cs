5e4c93a7e351e1ef10d3bd198bda4e9f31dec095
qanbarzadeh <ghxalireza@gmail.com>
2022-12-18 22:00:56 +0800

    Add TodoListService

--- Application.Tests/ToDoListServiceTest.cs
+++ Application.Tests/ToDoListServiceTest.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Application.Tests
 {
-    internal class ToDoListServiceTest
+    public class ToDoListServiceTest
     {
         [Fact]
         public Returns_NonPassedTasks()W
