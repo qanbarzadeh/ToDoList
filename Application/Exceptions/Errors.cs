4d496479d07c24e4b8beec7ad25bfad1bdd16b34
qanbarzadeh <ghxalireza@gmail.com>
2022-12-26 20:25:24 +0800

    Createed DueDateExeption

--- Application/Exceptions/Errors.cs
+++ Application/Exceptions/Errors.cs
@@ -1,12 +1,32 @@
 ﻿public class Errors
 {
+    /// <summary>
+    /// Failed to Create Task
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="TodoTaskException"></exception>
     public static Exception FailedToCreateDuedateTask()
     {
         throw new TodoTaskException("Failed to create duedate task witout a date");
     }
-
+    /// <summary>
+    /// Due date cannot be smaller than today's date
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="TodoTaskException"></exception>
     public static Exception NotFound(int id)
     {
         throw new TodoTaskException($"Task with  Id: {id} not found");
+    }
+
+    /// <summary>
+    /// Due date cannot be smaller than today's date
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="TodoTaskException"></exception>
+    public static Exception DueDateException()
+    {
+        throw new TodoTaskException ("Due date cannot be smaller than today's date");
     }
 }
