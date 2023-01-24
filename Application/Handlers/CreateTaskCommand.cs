2ed0691b4489d3654e120d8373fb4c61ea386a17
qanbarzadeh <ghxalireza@gmail.com>
2023-01-24 10:06:53 +0800

    Add MediatR classes for handling commands in feature/addMediatR branch

---
+++ Application/Handlers/CreateTaskCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR; // MediatR
+using Domain.Todo;
+
+namespace Application.Handlers
+    
+{
+    public class CreateTaskCommand : IRequest<TodoTask>
+    {
+        //public int id { get; set;  }
+        public string Title { get; set; }
+        //public string Completed { get; set; }
+        //public string Description { get; set; }
+        public string DueDate { get; set; }
+        
+        public CreateTaskCommand(string title, string dueDate)
+        {
+            //this.id = id;
+            this.Title = title;
+            //this.Completed = completed;
+            //this.Description = description;
+            this.DueDate = dueDate;
+        }
+    }
+}
---
+++ Application/Handlers/CreateTaskCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Todo;
+using MediatR; // MediatR
+namespace Application.Handlers
+{
+    public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand, BasicTask>
+    {
+        private readonly ITodoTasksService _todoTaskService; 
+
+        public CreateTaskCommandHandler(ITodoTasksService todoTasksService)
+        {
+            _todoTaskService = todoTasksService; 
+        }
+        public Task<TodoTask> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+        {
+            _todoTaskService.CreateTask(request, cancellationToken); 
+
+            
+        }
+    }
+}
